e21bd9a45eaecae117e18a4034da472dc8902b6b
Ledses <165549162+Ledses@users.noreply.github.com>
2024-11-03 15:50:20 +0500

    Add files via upload

---
+++ Stalin.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stalin : MonoBehaviour
+{
+
+    void Start()
+    {
+        print("арктика");
+    }
+
+    void Update()
+    {
+        
+    }
+}
